db9e373f4add1cd56d17bae952ee9a6d043f42fc
Dinh Dai <andinhdai@gmail.com>
2024-07-30 19:29:54 +0700

    cart

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,19 +1,62 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    //top-menu
     const topMenu = document.getElementById('add-top-menu')
     const menuToggle = document.getElementById('add-menu-toggle')
-        document.addEventListener('click', (e) => {
-            if (menuToggle.contains(e.target)) {
-                topMenu.classList.toggle('add-topmenu-expanded')
-                topMenu.classList.toggle('hidden')
-            } else{
-                if(topMenu.classList.contains('add-topmenu-expanded')){ 
+    document.addEventListener('click', (e) => {
+        if (menuToggle.contains(e.target)) {
+            topMenu.classList.toggle('add-topmenu-expanded')
+            topMenu.classList.toggle('hidden')
+        } else {
+            if (topMenu.classList.contains('add-topmenu-expanded')) {
                 topMenu.classList.remove('add-topmenu-expanded')
                 topMenu.classList.add('hidden')
-                }
             }
-        });
-        var video = document.getElementById('story-video');
-            video.muted = true;
-       
-    
+        }
+    });//end topmenu
+
+    //video
+    var video = document.getElementById('story-video');
+    video.muted = true;
+    //end video
+
+    //cart
+    const openCart = document.getElementById('Cart-toggle');
+    const closeCart = document.getElementById('close-cart');
+    const Cart = document.getElementById('add-cart');
+    function openCartWithAnimation() {
+        Cart.style.position = 'fixed'; // Thêm thuộc tính fixed
+        Cart.style.right = '0';
+        Cart.style.top = '0';
+        Cart.classList.remove('hidden');
+        Cart.classList.add('add-cart-show');
+        Cart.classList.remove('hidden');
+        Cart.classList.add('add-cart-show');
+        
+    }
+    function closeCartWithAnimation() {
+        Cart.classList.add('add-cart-hidden');
+        Cart.addEventListener('animationend', () => {
+            Cart.classList.add('hidden');
+            Cart.classList.remove('add-cart-hidden');
+            Cart.classList.remove('add-cart-show');
+        }, { once: true });
+    }
+    // Mở giỏ hàng khi nhấn vào Cart-toggle
+    openCart.addEventListener('click', (e) => {
+        e.stopPropagation(); 
+        openCartWithAnimation();
+    });
+
+    // Đóng giỏ hàng khi nhấn vào closeCart
+    closeCart.addEventListener('click', (e) => {
+        e.stopPropagation(); 
+        closeCartWithAnimation();
+    });
+    // Đóng giỏ hàng khi nhấn bên ngoài giỏ hàng
+    document.addEventListener('click', (e) => {
+        if (!Cart.contains(e.target) && !openCart.contains(e.target)) {
+            closeCartWithAnimation();
+        }
+    });
+    //end cart
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,12 +25,22 @@ module.exports = {
           '0%': { transform: 'translateY(0)' },
           '100%': { transform: 'translateY(-100%)' },
         },
+        slideOut:{
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideIn:{
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(100%)' },
+        },
         fadeIn:{
           from:{ opacity: 0}, to:{ opacity: 1}
         }
       },
       animation: {
         slideDown: 'slideDown 0.3s ease-out',
+        slideOut: 'slideOut 0.3s ease-in-out ',
+        slideIn: 'slideIn 0.3s ease-in-out ',
         fadeIn:' fadeIn 0.5s ease-in-out'
       },
       backgroundImage:{
